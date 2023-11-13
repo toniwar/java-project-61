9443d5c3cdb718988b359eeabf71e113984a9cb7
toniwar <toniuswar@gmail.com>
2023-11-13 15:09:28 +0300

    Changed gradle settings

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,9 @@
 plugins {
     id("java")
+    application
+}
+application{
+    mainClass = "hexlet.code.App"
 }
 
 group = "hexlet.code"
@@ -8,12 +12,17 @@ version = "1.0-SNAPSHOT"
 repositories {
     mavenCentral()
 }
-
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(20))
+    }
+}
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
-
 tasks.test {
     useJUnitPlatform()
 }
+
+
