a886380ef9207443f31bee696422e18d48eac9e0
toniwar <toniuswar@gmail.com>
2023-11-19 22:41:08 +0300

    Changed class Cli

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -1,13 +1,30 @@
 package hexlet.code;
 
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 public class Cli {
-    final static String GREETING = "Welcome to the Brain Games!\nMay I have You name?";
-    public static void greeting() {
+    public static String greeting() {
+        System.out.println("Welcome to the Brain Games!\nMay I have You name?");
+        String name;
+        while (true) {
+            name = fillInTheName();
+            if (!name.isBlank()) {
+                break;
+            }
+            System.out.println("The name cannot be empty. Please, enter Your name.");
+        }
+        System.out.printf("Hello, %s!\n", name);
+        return name;
+    }
+    public static String fillInTheName() {
         var scanner = new Scanner(System.in);
-        System.out.println(GREETING);
-        var userName = scanner.nextLine();
-        System.out.printf("Hello, %s!\n", userName);
+        String name;
+        try {
+            name = scanner.nextLine();
+        } catch (NoSuchElementException e) {
+            name = "";
+        }
+        return name;
     }
 }
