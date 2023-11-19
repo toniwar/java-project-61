5936f2f04776784fa875dbf5dcad5edbd2a2f4b4
toniwar <toniuswar@gmail.com>
2023-11-19 22:39:55 +0300

    Added game menu

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,36 @@
 package hexlet.code;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class App {
+    private static String player = "";
     public static void main(String[] args) {
-        Cli.greeting();
+        showGameMenu();
+    }
+    public static void showGameMenu() {
+        Scanner sc = new Scanner(System.in);
+        int answer = 0;
+        System.out.println("Please enter the game number and press Enter.");
+        System.out.println("1 - Greet\n2 - Even\n0 - Exit");
+        try {
+            answer = sc.nextInt();
+        } catch (InputMismatchException e) {
+            System.out.println("Incorrect input!");
+            showGameMenu();
+        }
+        System.out.println("Your choice: " + answer);
+        switch (answer) {
+            case 0 : return;
+            case 1 : player = Cli.greeting();
+            break;
+            case 2 : if (player.isBlank()) {
+                    player = Cli.greeting();
+                }
+                Game.oddOrEven(player);
+            break;
+            default: System.out.println("Incorrect input!");
+        }
+        showGameMenu();
     }
 }
