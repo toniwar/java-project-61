50c383737cf418406a0083eecbdb07ad5bb79f0c
toniwar <toniuswar@gmail.com>
2023-11-20 23:05:59 +0300

    Added GCD

---
+++ app/src/main/java/hexlet/code/games/GCD.java
@@ -0,0 +1,45 @@
+package hexlet.code.games;
+
+import java.util.Random;
+
+public class GCD implements Game {
+    private String rightAnswer;
+    @Override
+    public String showGameRule() {
+        return "Find the greatest common divisor of given numbers.";
+    }
+
+    @Override
+    public String generateQuestion() {
+        var random = new Random();
+        var x = generateEvenRandomNum(101);
+        var y = generateEvenRandomNum(51);
+        for (var i = y; i >= 1; i--) {
+            if (x % i == 0 && y % i == 0) {
+                rightAnswer = "" + i;
+                break;
+            }
+        }
+        return x + " " + y;
+    }
+
+    @Override
+    public boolean checkAnswer(String answer) {
+        return answer.equals(rightAnswer);
+    }
+
+    @Override
+    public String getRightAnswer() {
+        return rightAnswer;
+    }
+
+    private int generateEvenRandomNum(int limit) {
+        var random = new Random();
+        while (true) {
+            var num = random.nextInt(limit);
+            if (num % 2 == 0) {
+                return num;
+            }
+        }
+    }
+}
