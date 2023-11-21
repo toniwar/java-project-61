7ddf1a71a398c8a2895b001cc736fa8be4826e00
toniwar <toniuswar@gmail.com>
2023-11-21 03:36:15 +0300

    Added Prime number game

---
+++ app/src/main/java/hexlet/code/games/Prime.java
@@ -0,0 +1,43 @@
+package hexlet.code.games;
+
+import hexlet.code.games.repository.Game;
+
+import java.util.Random;
+
+public class Prime implements Game {
+    private String rightAnswer;
+
+    @Override
+    public String showGameRule() {
+        return "Answer 'yes' if given number is prime. Otherwise answer 'no'.";
+    }
+
+    @Override
+    public String generateQuestion() {
+        var num = new Random().nextInt(301);
+        rightAnswer = isPrime(num) ? "yes" : "no";
+        return "" + num;
+    }
+
+    @Override
+    public boolean checkAnswer(String answer) {
+        return answer.equalsIgnoreCase(rightAnswer);
+    }
+
+    @Override
+    public String getRightAnswer() {
+        return rightAnswer;
+    }
+
+    private boolean isPrime(int num) {
+        if (num < 2) {
+            return false;
+        }
+        for (var i = 2; i <= num / 2; i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
